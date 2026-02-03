9c0380e2693b9b34b79d2ecad17f87c10cc9b998
Oscar Doel Muñoz <Doncoca4686@gmail.com>
2026-02-02 20:37:27 -0600

    Creacion de la conexion

---
+++ src/app/services/orden.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Order, APIResponse } from '../Models/orders';
+import { Orden } from './orden';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdenService {
+  
+  // 🔴 ¡VERIFICA TU PUERTO AQUÍ!
+  private apiUrl = 'https://localhost:7153/api/Empleados'; 
+  // Nota: Si tu controller se llama OrdenesController, pon /api/Ordenes
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<APIResponse> {
+    return this.http.get<APIResponse>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Order> {
+    return this.http.get<Order>(`${this.apiUrl}/${id}`);
+  }
+
+  create(orden: Order): Observable<any> {
+    return this.http.post(this.apiUrl, orden);
+  }
+
+  update(orden: Order): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${orden.OrderID}`, orden);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
